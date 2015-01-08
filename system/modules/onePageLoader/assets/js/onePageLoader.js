b2a99cc045053f1e7c536e926bd7b9f61b0d027e
Sven Baumann <baumann.sv@gmail.com>
2015-01-08 11:29:15 +0100

    performance update
    add new functions:
    	loadSources,
    	parseSources,
    	md5,
    	complete
    
    remove or replace functions:
    	loadScripts,
    	pareScript

--- system/modules/onePageLoader/assets/js/onePageLoader.js
+++ system/modules/onePageLoader/assets/js/onePageLoader.js
@@ -16,7 +16,8 @@ var onePageLoader = function () {
 		outViewSection: '', /** TODO: outView Section **/
 		watchOffsetY: 0,
 		minHeightLastSection: true,
-		scrollToAcivePage: true,
+		scrollToActivePage: true,
+		completeTimeOut: 2000,
 		complete: function () {
 		}
 	};
@@ -24,7 +25,8 @@ var onePageLoader = function () {
 
 	var requests = 1,
 		scrollToSpan,
-		move;
+		move,
+		_cache = [];
 
 
 	function _setDefaultOption() {
@@ -91,8 +93,8 @@ var onePageLoader = function () {
 
 			var parent = realNode.parentNode;
 			var siblings = [];
-			_each(parent.children, function (i, element) {
-				element.tagName == name ? siblings.push(element) : '';
+			_each(parent.children, function () {
+				this.tagName == name ? siblings.push(this) : '';
 			});
 
 			siblings.length > 1 ? name += ':eq(' + siblings.index(node) + ')' : '';
@@ -146,7 +148,7 @@ var onePageLoader = function () {
 
 	function _parseSites(obj) {
 		var sites = [];
-		onePageLoader.each(obj, function (i, el) {
+		_each(obj, function (i, el) {
 			el.href && !el.hash ? sites.push(el) : '';
 
 			el.tagName === 'SPAN' && el.className.search('active') > -1 ? sites.push(el) : '';
@@ -253,11 +255,327 @@ var onePageLoader = function () {
 
 
 	function _handleByTagA(el) {
-		_loadSite(el, function (el, content, complete) {
-			complete === true ? _loadScripts() : '';
-			(_option.smoothScroll ? _initSmoothScroll(el) : false);
-			el.onePage.section.appendChild(content);
+		_loadSite(el, function (el, html, complete) {
+			_each(html.getElementsByTagName('script'), function () {
+				this ? _parseSources(this) : '';
+			});
+
+			_each(html.getElementsByTagName('link'), function () {
+				this ? _parseSources(this) : '';
+			});
+
+			_each(html.getElementsByTagName('style'), function () {
+				this ? _parseSources(this) : '';
+			});
+
+			_option.smoothScroll ? _initSmoothScroll(el) : false;
+
+			el.onePage.section.appendChild(html.querySelector(_option.body.selector).children[0]);
+
+			complete === true ? _loadSources() : '';
 		});
+	}
+
+
+	function _loadSources() {
+		var head,
+			body;
+
+		_cache.loadSources && _cache.loadSources.link ? control(_cache.loadSources.link) : '';
+		_cache.loadSources && _cache.loadSources.script ? control(_cache.loadSources.script) : '';
+		_cache.loadSources && _cache.loadSources.style ? control(_cache.loadSources.style) : '';
+
+		if (_cache.insert && _cache.insert.body && _cache.insert.body.length > 0) {
+			_each(_cache.insert.body, function () {
+				this.tagName === 'STYLE' || this.tagName === 'LINK' ? insertHead(this) : '';
+				this.tagName === 'SCRIPT' ? eval(this.text) : '';
+			});
+		}
+
+
+		function control(sources) {
+
+			if (sources.length > 0) {
+				for (var source in sources) {
+					var row = sources[source];
+					var insert = document.createElement(row.tagName.toLowerCase());
+
+					row.src ? insert.src = row.src : '';
+					row.href ? insert.href = row.href : '';
+					!insert.src || row.src === '' && !row.href ? insert.text = row.text : '';
+
+					insert.src || insert.href ? insertHead(insert) : '';
+					!insert.src || insert.src === ''  && !insert.href ? insertBody(insert) : '';
+				}
+			}
+		}
+
+
+		function insertHead(insert) {
+			head = head || document.getElementsByTagName('head')[0];
+
+			head.appendChild(insert);
+		}
+
+
+		function insertBody(insert) {
+			body = body || document.getElementsByTagName('body')[0];
+
+			_cache.insert = _cache.insert || {};
+			_cache.insert.body = _cache.insert.body || [];
+			_cache.insert.body.push(insert);
+		}
+	}
+
+
+	function _parseSources(source) {
+		if (!source.tagName) return false;
+
+		var type = source.tagName.toLowerCase();
+		if (!_cache.document || !_cache.document[type]) {
+			_cache.document = _cache.document || {};
+
+			if (!_cache.document[type]) {
+				_cache.document[type] = [];
+				_each(document.getElementsByTagName(type), function (i, el) {
+					var prop = el.href || el.src || el.innerHTML;
+					var hash = md5(prop);
+
+					_cache.document[type][hash] = el;
+					_cache.document[type].length++;
+				});
+			}
+		}
+
+		if (!_cache.loadSources || !_cache.loadSources[type]) {
+			_cache.loadSources = _cache.loadSources || [];
+			_cache.loadSources[type] = [];
+		}
+
+		var search = source.href || source.src || source.innerHTML;
+		var hash = md5(search);
+
+
+		if (!_cache.document[type][hash] && !_cache.loadSources[type][hash]) {
+			_cache.loadSources[type][hash] = source;
+			_cache.loadSources[type].length++;
+		}
+	}
+
+
+	function md5(string) {
+		function RotateLeft(lValue, iShiftBits) {
+			return (lValue << iShiftBits) | (lValue >>> (32 - iShiftBits));
+		}
+
+		function AddUnsigned(lX, lY) {
+			var lX4, lY4, lX8, lY8, lResult;
+			lX8 = (lX & 0x80000000);
+			lY8 = (lY & 0x80000000);
+			lX4 = (lX & 0x40000000);
+			lY4 = (lY & 0x40000000);
+			lResult = (lX & 0x3FFFFFFF) + (lY & 0x3FFFFFFF);
+			if (lX4 & lY4) {
+				return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
+			}
+			if (lX4 | lY4) {
+				if (lResult & 0x40000000) {
+					return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
+				} else {
+					return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
+				}
+			} else {
+				return (lResult ^ lX8 ^ lY8);
+			}
+		}
+
+		function F(x, y, z) {
+			return (x & y) | ((~x) & z);
+		}
+
+		function G(x, y, z) {
+			return (x & z) | (y & (~z));
+		}
+
+		function H(x, y, z) {
+			return (x ^ y ^ z);
+		}
+
+		function I(x, y, z) {
+			return (y ^ (x | (~z)));
+		}
+
+		function FF(a, b, c, d, x, s, ac) {
+			a = AddUnsigned(a, AddUnsigned(AddUnsigned(F(b, c, d), x), ac));
+			return AddUnsigned(RotateLeft(a, s), b);
+		}
+
+		function GG(a, b, c, d, x, s, ac) {
+			a = AddUnsigned(a, AddUnsigned(AddUnsigned(G(b, c, d), x), ac));
+			return AddUnsigned(RotateLeft(a, s), b);
+		}
+
+		function HH(a, b, c, d, x, s, ac) {
+			a = AddUnsigned(a, AddUnsigned(AddUnsigned(H(b, c, d), x), ac));
+			return AddUnsigned(RotateLeft(a, s), b);
+		}
+
+		function II(a, b, c, d, x, s, ac) {
+			a = AddUnsigned(a, AddUnsigned(AddUnsigned(I(b, c, d), x), ac));
+			return AddUnsigned(RotateLeft(a, s), b);
+		}
+
+		function ConvertToWordArray(string) {
+			var lWordCount;
+			var lMessageLength = string.length;
+			var lNumberOfWords_temp1 = lMessageLength + 8;
+			var lNumberOfWords_temp2 = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64;
+			var lNumberOfWords = (lNumberOfWords_temp2 + 1) * 16;
+			var lWordArray = Array(lNumberOfWords - 1);
+			var lBytePosition = 0;
+			var lByteCount = 0;
+			while (lByteCount < lMessageLength) {
+				lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+				lBytePosition = (lByteCount % 4) * 8;
+				lWordArray[lWordCount] = (lWordArray[lWordCount] | (string.charCodeAt(lByteCount) << lBytePosition));
+				lByteCount++;
+			}
+			lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+			lBytePosition = (lByteCount % 4) * 8;
+			lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80 << lBytePosition);
+			lWordArray[lNumberOfWords - 2] = lMessageLength << 3;
+			lWordArray[lNumberOfWords - 1] = lMessageLength >>> 29;
+			return lWordArray;
+		}
+
+		function WordToHex(lValue) {
+			var WordToHexValue = "", WordToHexValue_temp = "", lByte, lCount;
+			for (lCount = 0; lCount <= 3; lCount++) {
+				lByte = (lValue >>> (lCount * 8)) & 255;
+				WordToHexValue_temp = "0" + lByte.toString(16);
+				WordToHexValue = WordToHexValue + WordToHexValue_temp.substr(WordToHexValue_temp.length - 2, 2);
+			}
+			return WordToHexValue;
+		}
+
+		function Utf8Encode(string) {
+			string = string.replace(/\r\n/g, "\n");
+			var utftext = "";
+
+			for (var n = 0; n < string.length; n++) {
+				var c = string.charCodeAt(n);
+
+				if (c < 128) {
+					utftext += String.fromCharCode(c);
+				}
+				else if ((c > 127) && (c < 2048)) {
+					utftext += String.fromCharCode((c >> 6) | 192);
+					utftext += String.fromCharCode((c & 63) | 128);
+				}
+				else {
+					utftext += String.fromCharCode((c >> 12) | 224);
+					utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+					utftext += String.fromCharCode((c & 63) | 128);
+				}
+			}
+
+			return utftext;
+		}
+
+		var x = Array();
+		var k, AA, BB, CC, DD, a, b, c, d;
+		var S11 = 7, S12 = 12, S13 = 17, S14 = 22;
+		var S21 = 5, S22 = 9, S23 = 14, S24 = 20;
+		var S31 = 4, S32 = 11, S33 = 16, S34 = 23;
+		var S41 = 6, S42 = 10, S43 = 15, S44 = 21;
+
+		string = Utf8Encode(string);
+
+		x = ConvertToWordArray(string);
+
+		a = 0x67452301;
+		b = 0xEFCDAB89;
+		c = 0x98BADCFE;
+		d = 0x10325476;
+
+		for (k = 0; k < x.length; k += 16) {
+			AA = a;
+			BB = b;
+			CC = c;
+			DD = d;
+			a = FF(a, b, c, d, x[k + 0], S11, 0xD76AA478);
+			d = FF(d, a, b, c, x[k + 1], S12, 0xE8C7B756);
+			c = FF(c, d, a, b, x[k + 2], S13, 0x242070DB);
+			b = FF(b, c, d, a, x[k + 3], S14, 0xC1BDCEEE);
+			a = FF(a, b, c, d, x[k + 4], S11, 0xF57C0FAF);
+			d = FF(d, a, b, c, x[k + 5], S12, 0x4787C62A);
+			c = FF(c, d, a, b, x[k + 6], S13, 0xA8304613);
+			b = FF(b, c, d, a, x[k + 7], S14, 0xFD469501);
+			a = FF(a, b, c, d, x[k + 8], S11, 0x698098D8);
+			d = FF(d, a, b, c, x[k + 9], S12, 0x8B44F7AF);
+			c = FF(c, d, a, b, x[k + 10], S13, 0xFFFF5BB1);
+			b = FF(b, c, d, a, x[k + 11], S14, 0x895CD7BE);
+			a = FF(a, b, c, d, x[k + 12], S11, 0x6B901122);
+			d = FF(d, a, b, c, x[k + 13], S12, 0xFD987193);
+			c = FF(c, d, a, b, x[k + 14], S13, 0xA679438E);
+			b = FF(b, c, d, a, x[k + 15], S14, 0x49B40821);
+			a = GG(a, b, c, d, x[k + 1], S21, 0xF61E2562);
+			d = GG(d, a, b, c, x[k + 6], S22, 0xC040B340);
+			c = GG(c, d, a, b, x[k + 11], S23, 0x265E5A51);
+			b = GG(b, c, d, a, x[k + 0], S24, 0xE9B6C7AA);
+			a = GG(a, b, c, d, x[k + 5], S21, 0xD62F105D);
+			d = GG(d, a, b, c, x[k + 10], S22, 0x2441453);
+			c = GG(c, d, a, b, x[k + 15], S23, 0xD8A1E681);
+			b = GG(b, c, d, a, x[k + 4], S24, 0xE7D3FBC8);
+			a = GG(a, b, c, d, x[k + 9], S21, 0x21E1CDE6);
+			d = GG(d, a, b, c, x[k + 14], S22, 0xC33707D6);
+			c = GG(c, d, a, b, x[k + 3], S23, 0xF4D50D87);
+			b = GG(b, c, d, a, x[k + 8], S24, 0x455A14ED);
+			a = GG(a, b, c, d, x[k + 13], S21, 0xA9E3E905);
+			d = GG(d, a, b, c, x[k + 2], S22, 0xFCEFA3F8);
+			c = GG(c, d, a, b, x[k + 7], S23, 0x676F02D9);
+			b = GG(b, c, d, a, x[k + 12], S24, 0x8D2A4C8A);
+			a = HH(a, b, c, d, x[k + 5], S31, 0xFFFA3942);
+			d = HH(d, a, b, c, x[k + 8], S32, 0x8771F681);
+			c = HH(c, d, a, b, x[k + 11], S33, 0x6D9D6122);
+			b = HH(b, c, d, a, x[k + 14], S34, 0xFDE5380C);
+			a = HH(a, b, c, d, x[k + 1], S31, 0xA4BEEA44);
+			d = HH(d, a, b, c, x[k + 4], S32, 0x4BDECFA9);
+			c = HH(c, d, a, b, x[k + 7], S33, 0xF6BB4B60);
+			b = HH(b, c, d, a, x[k + 10], S34, 0xBEBFBC70);
+			a = HH(a, b, c, d, x[k + 13], S31, 0x289B7EC6);
+			d = HH(d, a, b, c, x[k + 0], S32, 0xEAA127FA);
+			c = HH(c, d, a, b, x[k + 3], S33, 0xD4EF3085);
+			b = HH(b, c, d, a, x[k + 6], S34, 0x4881D05);
+			a = HH(a, b, c, d, x[k + 9], S31, 0xD9D4D039);
+			d = HH(d, a, b, c, x[k + 12], S32, 0xE6DB99E5);
+			c = HH(c, d, a, b, x[k + 15], S33, 0x1FA27CF8);
+			b = HH(b, c, d, a, x[k + 2], S34, 0xC4AC5665);
+			a = II(a, b, c, d, x[k + 0], S41, 0xF4292244);
+			d = II(d, a, b, c, x[k + 7], S42, 0x432AFF97);
+			c = II(c, d, a, b, x[k + 14], S43, 0xAB9423A7);
+			b = II(b, c, d, a, x[k + 5], S44, 0xFC93A039);
+			a = II(a, b, c, d, x[k + 12], S41, 0x655B59C3);
+			d = II(d, a, b, c, x[k + 3], S42, 0x8F0CCC92);
+			c = II(c, d, a, b, x[k + 10], S43, 0xFFEFF47D);
+			b = II(b, c, d, a, x[k + 1], S44, 0x85845DD1);
+			a = II(a, b, c, d, x[k + 8], S41, 0x6FA87E4F);
+			d = II(d, a, b, c, x[k + 15], S42, 0xFE2CE6E0);
+			c = II(c, d, a, b, x[k + 6], S43, 0xA3014314);
+			b = II(b, c, d, a, x[k + 13], S44, 0x4E0811A1);
+			a = II(a, b, c, d, x[k + 4], S41, 0xF7537E82);
+			d = II(d, a, b, c, x[k + 11], S42, 0xBD3AF235);
+			c = II(c, d, a, b, x[k + 2], S43, 0x2AD7D2BB);
+			b = II(b, c, d, a, x[k + 9], S44, 0xEB86D391);
+			a = AddUnsigned(a, AA);
+			b = AddUnsigned(b, BB);
+			c = AddUnsigned(c, CC);
+			d = AddUnsigned(d, DD);
+		}
+
+		var temp = WordToHex(a) + WordToHex(b) + WordToHex(c) + WordToHex(d);
+
+		return temp.toLowerCase();
 	}
 
 
@@ -357,57 +675,6 @@ var onePageLoader = function () {
 	}
 
 
-	function _loadScripts() {
-		var scriptsLength = onePageLoader.scripts.length;
-		var head = document.getElementsByTagName('head')[0];
-		_each(onePageLoader.scripts, function (i, el) {
-			if (el.src) {
-				var script = document.createElement('script');
-				script.src = el.src;
-				head.appendChild(script);
-			}
-			if (i === scriptsLength - 1 || scriptsLength === 0) {
-				window.setTimeout(function () {
-					_each(onePageLoader.scripts, function (i, el) {
-						if (el.innerHTML) {
-							try {
-								eval(el.innerHTML);
-							}
-							catch (e) {
-								console.error('Javascript function eval() ERROR');
-								console.error(el.innerHTML);
-							}
-							if (i === scriptsLength - 1 || scriptsLength === 0) {
-								if (_option.minHeightLastSection) {
-									_lastSectionMinHeight();
-									_bind(window, 'resize', function () {
-										_lastSectionMinHeight();
-									})
-								}
-								window.setTimeout(function () {
-									_viewInSection();
-									_option.scrollToAcivePage ? _scrollToActivePage() : '';
-									onePageLoader.complete();
-									if (onePageLoader.css.length > 0) {
-										_each(onePageLoader.css, function (i, el) {
-											if (document.querySelectorAll('link[href="' + el.href + '"]').length == 0) {
-												var css = document.createElement('link');
-												css.rel = el.rel;
-												css.href = el.href;
-												head.appendChild(css);
-											}
-										});
-									}
-								}, 200);
-							}
-						}
-					});
-				}, 500);
-			}
-		});
-	}
-
-
 	function _lastSectionMinHeight() {
 		var el = document.querySelectorAll('.' + _option.siteName + '_container');
 		var height = window.innerHeight || window.screen.availHeight;
@@ -499,59 +766,26 @@ var onePageLoader = function () {
 	function _loadSite(el, callback) {
 		var request;
 		var complete = false;
+
 		if (window.ActiveXObject) {
 			request = new ActiveXObject('Microsoft.XMLHTTP');
 		} else {
 			request = new XMLHttpRequest();
 		}
+
 		request.onreadystatechange = function () {
 			if (request.readyState === 4) {
 				var html = document.createElement('div');
 				html.innerHTML = request.responseText;
 
-				_each(html.getElementsByTagName('script'), function (i, script) {
-					_parseScript(script) === true ? onePageLoader.scripts.push(script) : '';
-				});
-
-				_each(html.getElementsByTagName('link'), function (i, css) {
-					_parseScript(css) === true ? onePageLoader.css.push(css) : '';
-				});
-
 				requests++;
 
 				onePageLoader.sites.length === requests ? complete = true : false;
-				callback.call(html.querySelector(_option.body.selector).children[0], el, html.querySelector(_option.body.selector).children[0], complete);
+				callback.call(html, el, html, complete);
 			}
 		};
 		request.open('GET', el.href.replace(el.hash, ''));
 		request.send();
-	}
-
-
-	function _parseScript(obj) {
-		var loaded = false,
-			type;
-
-		obj.src ? type = 'src' : '';
-		obj.href ? type = 'href' : '';
-		obj.innerHTML ? type = 'innerHTML' : '';
-
-		_each(onePageLoader.scripts, function (i, el) {
-			(obj[type] === el[type] ? loaded = true : false);
-		});
-
-		if (loaded == false) {
-			if (type === 'src' || type === 'href') {
-				loaded = true;
-
-				_each(document.querySelectorAll(obj.tagName), function (iHTML, elHTML) {
-					(elHTML[type] == obj[type] ? loaded = false : true);
-				});
-				return loaded != false;
-			}
-			return true;
-		}
-		return false;
 	}
 
 
@@ -571,12 +805,33 @@ var onePageLoader = function () {
 	}
 
 
+	function _complete() {
+		_bind(window, 'load', function () {
+			if (_option.minHeightLastSection) {
+				_lastSectionMinHeight();
+				_bind(window, 'resize', function () {
+					_lastSectionMinHeight();
+				})
+			}
+
+			_viewInSection();
+
+			_option.scrollToActivePage ? _scrollToActivePage() : '';
+
+			window.setTimeout(function() {
+				onePageLoader.complete();
+			}, _option.completeTimeOut);
+		});
+	}
+
+
 	return {
 		option: _setDefaultOption(),
 		init: function (obj, option) {
 			_setOption(option);
 			_setSites(obj);
 			_init();
+			_complete();
 		},
 
 
